32660e25b5af65f690ea98885b28aa8f2d494569
Eric Fakhourian <ericfa@Erics-MacBook-Pro-10.local>
2016-06-17 11:57:54 -0700

    Tap game

--- tapIt/ViewController.swift
+++ tapIt/ViewController.swift
@@ -9,16 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //properties 
+    var maxTaps = 0
+    var currentTaps = 0
+    
+   
+    //outlates
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var howManyTaps: UITextField!
+    @IBOutlet weak var playButt: UIButton!
+    
+    @IBOutlet weak var tapButt: UIButton!
+    @IBOutlet weak var tabLaper: UILabel!
+    
+    @IBAction func onCoinTapped(sender: UIButton!){
+        currentTaps += 1
+        updateTapsLBL()
+        
+        if isGameOver(){
+            resturtGame()
+            
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onPlayerButtonPressed(sender: UIButton!){
+        
+        
+        if howManyTaps.text != nil && howManyTaps.text != ""{
+            logoImage.hidden = true
+            playButt.hidden = true
+            howManyTaps.hidden = true
+            
+            tapButt.hidden = false
+            tabLaper.hidden = false
+            
+            maxTaps = Int(howManyTaps.text!)!
+            currentTaps = 0
+            
+            updateTapsLBL()
+            
+        }
+        
+        
+        
     }
+    
+    func resturtGame(){
+        maxTaps = 0
+        howManyTaps.text = ""
+        
+        logoImage.hidden = false
+        playButt.hidden = false
+        howManyTaps.hidden = false
+        
+        tapButt.hidden = true
+        tabLaper.hidden = true
+    }
+    
+    func isGameOver()-> Bool{
+        if currentTaps >= maxTaps{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func updateTapsLBL() {
+        
+        tabLaper.text = "\(currentTaps) Taps"
+
+    }
+    
+    
 
 
 }
